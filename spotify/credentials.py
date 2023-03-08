bbb175c2d57e461eaecd1fb91e84c0bb598ccc00
vivian347 <vkinyua001@gmail.com>
2023-03-08 06:22:02 +0300

    authorization flow

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,4 @@
 CLIENT_ID = "7aa3ef3d5a974d118717782d2847e9d4"
 CLIENT_SECRET = "836b35c8975848528b7a32efdc560723"
-REDIRECT_URI = "https://melomeet-5rs2.onrender.com/spotify/redirect"
+REDIRECT_URI = ["https://melomeet-5rs2.onrender.com/spotify/redirect",
+                "http://127.0.0.1:8000/spotify/redirect"]
